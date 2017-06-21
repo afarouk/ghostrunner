4a45bea0c3bfa050e5dbab0c86dbd54a66b7ba4e
Vasiliy Shevchuk <vasiliyshevchuk@Vasiliys-iMac.local>
2017-06-21 15:14:56 +0300

    team create changes

--- app/app_ghostrunner/game/scripts/views/partials/teamPlayersList.js
+++ app/app_ghostrunner/game/scripts/views/partials/teamPlayersList.js
@@ -3,29 +3,55 @@
 'use strict';
 
 define([
-	'ejs!../../templates/partials/teamPlayer.ejs'
-	], function(template){
-	var TeamPlayerView = Mn.View.extend({
+	'ejs!../../templates/partials/teamFielder.ejs',
+	'ejs!../../templates/partials/teamPitcher.ejs'
+	], function(fielderTmpl, pitcherTmpl){
+	//FILDER
+	var TeamPlayerFielderView = Mn.View.extend({
 		tagName: 'tr',
 		className: 'team-player',
-		template: template,
+		template: fielderTmpl,
 		ui: {
 			select: '[name="select-player"]',
 		},
 		triggers: {
 			'change @ui.select': 'selection:changed',
 		},
-		serializeData: function() {
-			return _.extend(this.model.toJSON(), {
-				positions: this.options.positions
-			});
-		}
+		// serializeData: function() {
+		// 	return _.extend(this.model.toJSON(), {
+		// 		positions: this.options.positions
+		// 	});
+		// }
 	});
-
+	//PITCHER
+	var TeamPlayerPitcherView = Mn.View.extend({
+		tagName: 'tr',
+		className: 'team-player',
+		template: pitcherTmpl,
+		ui: {
+			select: '[name="select-player"]',
+		},
+		triggers: {
+			'change @ui.select': 'selection:changed',
+		},
+		// serializeData: function() {
+		// 	return _.extend(this.model.toJSON(), {
+		// 		positions: this.options.positions
+		// 	});
+		// }
+	});
+	//PLAYERS LIST
 	var TeamPlayersListView = Mn.CollectionView.extend({
 		className: 'team-players',
 		tagName: 'tbody',
-		childView: TeamPlayerView,
+		childView: function(model) {
+			var type = model.get('type').enumText;
+			if (type === 'FIELDER') {
+				return TeamPlayerFielderView;
+			} else {
+				return TeamPlayerPitcherView;
+			}
+		},
 		childViewOptions: function() {
 			return this.options;
 		},
