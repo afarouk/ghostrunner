94051ec8a119c79f42f9d7a64ddba2ae9826255d
rrepaka <rrepaka@gmail.com>
2017-07-27 15:15:56 -0700

    Banner changes
    Banner changes

--- app/app_ghostrunner/page/navbar.php
+++ app/app_ghostrunner/page/navbar.php
@@ -5,8 +5,8 @@
 
                 <div class="navbar-brand">
                 	<a href="index.php">
-						<span class="nav_logo_one"><img src="images/mbb_ball_long.png" align="left" /></span>
-						<span class="nav_logo_two"><img src="images/banner.png" alt="Logo" /></span>
+						<span class="nav_logo_one"><img src="images/banner.png" align="left" /></span>
+						<span class="nav_logo_two"><img src="images/MatchUp_Baseball.png" alt="Logo" /></span>
                 	</a> <!-- site logo -->
                 </div>
                 <ul class="nav navbar-nav">
