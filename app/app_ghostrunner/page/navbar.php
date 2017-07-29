c5b5f48168814e6ff70d38e020d0f107a80b8154
rrepaka <rrepaka@gmail.com>
2017-07-29 11:32:46 -0700

    Added a new baseball logo
    Added a new baseball logo

--- app/app_ghostrunner/page/navbar.php
+++ app/app_ghostrunner/page/navbar.php
@@ -6,7 +6,7 @@
                 <div class="navbar-brand">
                 	<a href="index.php">
 						<span class="nav_logo_one"><img src="images/banner.png" align="left" /></span>
-						<span class="nav_logo_two"><img src="images/MatchUp_Baseball.png" alt="Logo" /></span>
+						<span class="nav_logo_two"><img src="images/MatchUp_Baseball_long.png" alt="Logo" /></span>
                 	</a> <!-- site logo -->
                 </div>
                 <ul class="nav navbar-nav">
