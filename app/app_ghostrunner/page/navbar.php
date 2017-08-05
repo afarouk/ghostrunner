de46176ab8382084f0aa08ed997e469e4467a4b1
Alamgir Farouk <afarouk2@hotmail.com>
2017-08-04 22:25:00 -0700

    i have a code

--- app/app_ghostrunner/page/navbar.php
+++ app/app_ghostrunner/page/navbar.php
@@ -10,8 +10,8 @@
                 	</a> <!-- site logo -->
                 </div>
                 <ul class="nav navbar-nav">
-                	<!--<li><a href="index.php#header" class="smooth-scroll">Home</a></li>
-                    <li><a href="index.php#contact" class="smooth-scroll">Contact</a></li>-->
+                	 <!-- <li><a href="index.php#header" class="smooth-scroll">Home</a></li>-->
+                    <li><a href="index.php#contact" class="smooth-scroll">I have a code</a></li>
                     <li><span>&nbsp;&nbsp;&nbsp;&nbsp;</span></li>
                     <li><span class="show_userName" href="javascript:void(0)"></span></li>
                     <li class="menu-btn"><a class="login-btn" href="#">Login</a></li>
