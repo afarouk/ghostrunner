377a3d54a25aa1860dce6738f91113f29a4ff198
vasiliyhop <vasiliyhop@gmail.com>
2017-10-13 10:28:21 +0300

    selectors changes

--- app/app_ghostrunner/game/scripts/views/partials/lineUpPlayersList.js
+++ app/app_ghostrunner/game/scripts/views/partials/lineUpPlayersList.js
@@ -47,36 +47,22 @@ define([
 				properties:values,
 				positions: this.model.get('positions')
 			};
+		},
+		onEnableBOSelector: function(enable) {
+			if (enable) {
+				this.ui.bo.attr('disabled', false);
+			} else {
+				this.ui.bo
+					.attr('disabled', true)
+					.find('option:eq(0)').prop('selected', true).change();
+				// this.ui.bo.change();
+			}
 		}
 	});
 
-	// var LineUpPitcherView = Mn.View.extend({
-	// 	tagName: 'tr',
-	// 	className: 'lineUp-player pitcher',
-	// 	template: pitherTmpl,
-	// 	ui: {
-	// 		select: '[name="select-player"]',
-	// 		rp: '[name="pitcher-role"]'
-	// 	},
-	// 	triggers: {
-	// 		'change @ui.select': 'selection:changed',
-	// 		'change @ui.rp': 'pitcherRole:changed'
-	// 	},
-	// 	serializeData: function() {
-	// 		var starterModel = this.options.lineUp.at(0),
-	// 			starter = this.model.get('playerId') === starterModel.get('playerId') &&
-	// 				this.model.get('seasonId') === starterModel.get('seasonId') ? true : false;
-	// 		if (starter) this.$el.addClass('starter-player');
-	// 		return _.extend(this.model.toJSON(), {
-	// 			starter: starter,
-	// 			// positions: this.options.positions
-	// 		});
-	// 	}
-	// });
-
 	var LineUpPlayersListView = Mn.CompositeView.extend({
 		template: tableTmpl,
-		className: 'lineUp-players',
+		className: 'lineUp-players without-checkbox',
 		// tagName: 'tbody',
 		childView: LineUpFielderView,
 		childViewContainer: "tbody",
@@ -104,12 +90,14 @@ define([
 
 			if (position) {
 				model.set('position', position);
+				view.triggerMethod('enableBOSelector', true);
 			} else {
 				model.set('position', {
 					displayText: 'UU',
 					enumText: 'UNDEFINED',
 					id: 0
 				});
+				view.triggerMethod('enableBOSelector', false);
 			}
 			this.checkIfEnable(view, model);
 			this.children.each(function(childView) {
@@ -187,63 +175,4 @@ define([
 		}
 	});
 	return LineUpPlayersListView;
-
-	// var LineUpPlayersListView = Mn.CollectionView.extend({
-	// 	className: 'lineUp-players',
-	// 	tagName: 'tbody',
-	// 	childView: function(model) {
-	// 		var type = model.get('type').enumText;
-	// 		if (type === 'FIELDER') {
-	// 			return LineUpFielderView;
-	// 		} else {
-	// 			return LineUpPitcherView;
-	// 		}
-	// 	},
-	// 	childViewOptions: function() {
-	// 		return this.options;
-	// 	},
-	// 	onChildviewSelectionChanged: function(view, e) {
-	// 		var checked = $(e.currentTarget).is(':checked'),
-	// 			model = view.model;
-
-	// 		this.trigger('lineUp:changed', checked, model);
-	// 	},
-	// 	onChildviewPositionChanged: function(view, e) {
-	// 		var $target = $(e.currentTarget),
-	// 			model = view.model,
-	// 			selected = $target.find(':selected').val(),
-	// 			positions = model.get('positions'),
-	// 			position = _.findWhere(positions, {enumText: selected});
-	// 		model.set('position', position);
-	// 	},
-	// 	onChildviewBattingOrderChanged: function(view, e) {
-	// 		var $target = $(e.currentTarget),
-	// 			model = view.model,
-	// 			selected = $target.find(':selected').val();
-	// 		model.set('battingOrder', selected);
-	// 	},
-	// 	onChildviewPitcherRoleChanged: function(view, e) {
-	// 		var $target = $(e.currentTarget),
-	// 			model = view.model,
-	// 			selected = $target.find(':selected').val(),
-	// 			roles = model.get('pitcherRoles'),
-	// 			role = _.findWhere(roles, {enumText: selected});
-	// 		model.set('pitcherRole', role);
-	// 	},
-	// 	onPlayersSelectionAllow: function(allow) {
-	// 		this.children.each(function(view) {
-	// 			var $checkbox = view.ui.select,
-	// 				isChecked = $checkbox.is(':checked');
-	// 			if (isChecked) {
-	// 				$checkbox.attr('disabled', false);
-	// 			} else {
-	// 				if (allow) {
-	// 					$checkbox.attr('disabled', false);
-	// 				} else {
-	// 					$checkbox.attr('disabled', true);
-	// 				}
-	// 			}
-	// 		});
-	// 	}
-	// });
 });
