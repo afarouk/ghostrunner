daa0b62aa8e79d2640707a01c0222dc292fc6268
Alamgir Farouk <afarouk2@hotmail.com>
2018-02-10 16:09:56 -0800

    Update icons

--- app/app_ghostrunner/page/header.php
+++ app/app_ghostrunner/page/header.php
@@ -10,24 +10,61 @@
 ?>
 <!DOCTYPE html>
 
+
 <head>
+  <title>GhostRunner</title>
+  <meta name="description" content="" />
+  <meta name="keywords" content="" />
+  <meta name="author" content="GhostRunner" />
+  <!-- splash screen for IOS, but apple hasn't fixed their bugs -->
+  <link rel="apple-touch-startup-image" href="Default-portrait@2x~iphone5.png">
 
-    <title>GhostRunner</title>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta name="author" content="GhostRunner" />
+  <!-- web-app settings -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 
-    <!-- Mobile Specific Meta -->
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="mobile-web-app-capable" content="yes" />
+  <!-- App Icon or WebClip icon -->
+  <link rel="apple-touch-icon"  href="images/apple-touch-icon-192x192.png" />
+  <link rel="icon" sizes="192x192" href="images/apple-touch-icon-192x192.png" />
 
+  <link rel="apple-touch-icon" href="images/apple-touch-icon-192x192.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="192x192" href="images/apple-touch-icon-192x192.png" />
+
+  <!-- favIcon -->
+  <link rel="icon" href="images/favicon.ico" />
+  <link rel="shortcut icon" href="images/favicon.ico" />
+
+  <!-- Sharing meta data for Facebook / Twitter etc -->
+  <meta name="description" content="GhostRunner" />
+  <meta name="keywords" content="GhostRunner" />
+  <meta name="author" content="GhostRunner.net" />
+
+
+  <meta property="og:type"               content="article" />
+  <meta property="og:title"              content=""/>
+  <meta property="og:description"        content=""/>
+  <meta property="og:image"              content=""/>
+  <meta property="og:url"                content=""/>
+
+  <meta name="twitter:card"              content=""/>
+  <meta name="twitter:site"              content=""/>
+  <meta name="twitter:creator"           content=""/>
+  <meta name="twitter:title"             content=""/>
+  <meta name="twitter:description"       content=""/>
+  <meta name="twitter:image"             content=""/>
+  <meta name="twitter:url"               content=""/>
+
+  <!--  End sharing meta data -->
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Poppins:400,300,600,700" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,300,600,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
 
     <!-- Stylesheets -->
-    <link rel="stylesheet" href="./bundle.css">
+    <link rel="stylesheet" href="./bundle.css" />
 
     <style  type="text/css">
         <?php
@@ -41,12 +78,8 @@
 
     <noscript><link rel="stylesheet" href="css/no-js.css"></noscript>
 
-    <!-- Favicons -->
-	<link rel="shortcut icon" href="images/favicon.ico">
-	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
-    
+
+
     <!-- js for facebook login-->
     <script src="https://connect.facebook.net/en_US/sdk.js"></script>
     <!-- js for facebook login-->
@@ -61,10 +94,7 @@
           });
         }
         window.ghostrunner = {};
-        window.ghostrunner.isDesktop = <?php echo  $isDesktop==TRUE ? 'true':'false' ?>;
+        window.ghostrunner.isDesktop = <?php echo  $isDesktop==true ? 'true':'false' ?>;
     </script>
 	<script src="http://cdn.ckeditor.com/4.5.5/standard/ckeditor.js"></script>
 </head>
-<body>
-   
-   
